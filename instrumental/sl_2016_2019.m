f68682c63c3642756536dea0d23d6d16aca104fa
aredondas <aredondas@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2019-08-09 13:58:14 +0000

    colecta tables for several years

--- instrumental/sl_2016_2019.m
+++ instrumental/sl_2016_2019.m
@@ -2,6 +2,8 @@ sl=cell(3,1);sl_cr=cell(3,1);sl_raw=cell(3,1)
 brewer=[157,183,185]
 for i=1:3
     sl{i}=[];
+    sl{i}.data=[];
+    sl{i}.events={};
     sl_cr{i}=[];
 for ano=2016:2019
     j=ano-2015;
@@ -9,8 +11,18 @@ for ano=2016:2019
    if exist(s1_)
      s=load(s1_);
      sl_raw{i}=[sl_raw{i};s.sl_rw];
-     sl{i,j}=s.tabla_tc;
+     %sl{i,j}=s.tabla_tc;
+     sl{i}.data=[sl{i}.data;s.tabla_tc.data];
+     sl{i}.events=[sl{i}.events,s.tabla_tc.events];
+     
    end
-   
+  
 end
+
+   t_temperature=array2table(sl{i}.data,'VariableNames',[{'date'},str2name((s.tabla_tc.data_lbl))],'RowNames',varname(sl{i}.events));
+   t_temperature.Date_str=datestr(t_temperature.date,'yyyy/mm/dd');
+   t_temperature=t_temperature(:,[end,1:end-1])
+   writetable(t_temperature,strrep('temp_157.xls','157',num2str(brewer(i))),'WriteRowNames',true)
+
+
 end
